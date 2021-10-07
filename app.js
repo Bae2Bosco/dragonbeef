f9da423ac9b70a5a8d138a3902b1b22e094cad10
Bae2Bosco <89792104+Bae2Bosco@users.noreply.github.com>
2021-10-07 04:39:45 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var ele = document.querySelector(".looper");
 
 
-var sum = Number(prompt("How many attempts do you intend to hit the dragon?"));
-for(var i=0; i<5; i++){
-    sum=sum+1
+var slaying = true;
+var youAtk = Math.floor(Math.random()*2);
+var yourDamage = Math.floor(Math.random()*5+1);
+var totalDamage = 0;
+
+
+while(slaying){
+    var question = (prompt("How many hit you attempting to hit the dragon?"))
+    if (youAtk <=5){
+        console.log("You hit the dragon!");
+        totalDamage += yourDamage;
+        if (totalDamage >= 9){
+           console.log("You have defeated the dragon!")
+            slaying = false;
+        }else {
+            youAtk = Math.floor(Math.random()*2);
+        }
+    }else{
+        if (yourDamage >=4)
+            console.log("The dragon has defeated you!");
+    }
+    slaying = false;
 }
 
-ele.innerHTML=sum;
