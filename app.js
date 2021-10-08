1c2ec180c7a1cd9f02bdf5d743e17cbfd7b6bd29
Bae2Bosco <89792104+Bae2Bosco@users.noreply.github.com>
2021-10-08 00:19:47 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ var totalDamage = 0;
 while(slaying){
     var question = (prompt("How many hit you attempting to hit the dragon?"))
     if (youAtk <=5){
-        console.log("You hit the dragon!");
+        ele.innerHTML="You hit the dragon!";
         totalDamage += yourDamage;
         if (totalDamage >= 9){
-           console.log("You have defeated the dragon!")
+           ele.innerHTML="You have defeated the dragon!";
             slaying = false;
         }else {
             youAtk = Math.floor(Math.random()*2);
