f3f0758f4663f412832d6a4b34f80c596a1547e2
Bae2Bosco <89792104+Bae2Bosco@users.noreply.github.com>
2021-10-10 23:29:04 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -3,25 +3,108 @@ var ele = document.querySelector(".looper");
 
 var slaying = true;
 var youAtk = Math.floor(Math.random()*2);
-var yourDamage = Math.floor(Math.random()*5+1);
 var totalDamage = 0;
+var playerHp = 5;
+var dragonHp = 10;
+var fire = 3;
+var ice = 1;
+var poison = 4;
 
 
-while(slaying){
-    var question = (prompt("How many hit you attempting to hit the dragon?"));
-    if (question <=5){
-        ele.innerHTML="You hit the dragon!";
-        totalDamage += yourDamage;
-        if (totalDamage >= 9){
-           ele.innerHTML="You have defeated the dragon!";
+
+let btn = document.createElement("button");
+btn.innerHTML = "Fire";
+while(slaying) {
+    var yourDamage = Math.floor(Math.random() * 5 + 1);
+    if (youAtk <= 5) {
+        fire += yourDamage;
+        ele.innerHTML = "You hit the dragon!";
+        ele.innerHTML = "";
+        dragonHp -= totalDamage
+        if (dragonHp <= 0) {
+            ele.innerHTML = "You have defeated the dragon!";
             slaying = false;
-        }else {
+        } else {
+            totalDamage = 0;
+            youAtk = Math.floor(Math.random() * 2);
+        }
+    } else {
+        totalDamage += yourDamage;
+        ele.innerHTML = "The dragon hit you!";
+        ele.innerHTML = "";
+        if (playerHp <= 0) {
+            ele.innerHTML = "The dragon has defeated you!";
+            slaying = false;
+        }else{
+            totalDamage=0;
             youAtk = Math.floor(Math.random()*2);
         }
-    }else{
-        if (yourDamage >=4)
-            ele.innerHTML="The dragon has defeated you!";
     }
-    slaying = false;
 }
 
+
+
+var btn2 = document.createElement("button")
+btn2.innerHTML = "Ice";
+while(slaying) {
+    var yourDamage2 = Math.floor(Math.random() * 5 + 1);
+    if (youAtk <= 5) {
+        ice += yourDamage2;
+        ele.innerHTML = "You hit the dragon!";
+        ele.innerHTML = "";
+        dragonHp -= totalDamage
+        if (dragonHp <= 0) {
+            ele.innerHTML = "You have defeated the dragon!";
+            slaying = false;
+        } else {
+            totalDamage = 0;
+            youAtk = Math.floor(Math.random() * 2);
+        }
+    } else {
+        totalDamage += yourDamage2;
+        ele.innerHTML = "The dragon hit you!";
+        ele.innerHTML = "";
+        if (playerHp <= 0) {
+            ele.innerHTML = "The dragon has defeated you!";
+            slaying = false;
+        }else{
+            totalDamage=0;
+            youAtk = Math.floor(Math.random()*2);
+        }
+    }
+}
+
+var btn3 = document.createElement("button");
+btn3.innerHTML = "Poison";
+while(slaying) {
+    var yourDamage3 = Math.floor(Math.random() * 5 + 1);
+    if (youAtk <= 5) {
+        poison += yourDamage3;
+        ele.innerHTML = "You hit the dragon!";
+        ele.innerHTML = "";
+        dragonHp -= totalDamage
+        if (dragonHp <= 0) {
+            ele.innerHTML = "You have defeated the dragon!";
+            slaying = false;
+        } else {
+            totalDamage = 0;
+            youAtk = Math.floor(Math.random() * 2);
+        }
+    } else {
+        totalDamage += yourDamage3;
+        ele.innerHTML = "The dragon hit you!";
+        ele.innerHTML = "";
+        if (playerHp <= 0) {
+            ele.innerHTML = "The dragon has defeated you!";
+            slaying = false;
+        }else{
+            totalDamage=0;
+            youAtk = Math.floor(Math.random()*2);
+        }
+    }
+}
+
+
+document.body.appendChild(btn);
+document.body.appendChild(btn2);
+document.body.appendChild(btn3);
